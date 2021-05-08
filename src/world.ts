7f05e8ff2c9a59afb7c60a934b774e9b60ce2725
kohei-jp <norwaymi38@gmail.com>
2021-05-08 19:27:39 +0900

    CI設定

--- src/world.ts
+++ src/world.ts
@@ -1,17 +1,18 @@
 export default class World {
-  message: string;
+  // クラスで使うプロパティ
+  message: string
 
+  // コンストラクタ（初期化）
   constructor(message: string) {
     this.message = message
   }
 
-  public sayHello(elem: HTMLElement | null): void {
-    if (elem){
-      elem.innerText = this.message
+  // 外部から呼び出せるメソッドを定義
+  public sayHello(elem: HTMLElement | null) {
+    if (elem) {
+      return (elem.innerText = this.message)
+    } else {
+      return
     }
-
-
-
-    
   }
 }
